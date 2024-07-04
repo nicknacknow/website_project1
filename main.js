3638102ff1e13c852e78f3ab03789d4dca7271b0
nick <nick.notaweeb@gmail.com>
2024-07-04 23:51:51 +0100

    Designing UX interactions

---
+++ main.js
@@ -0,0 +1,20 @@
+/*const menu_button = document.querySelector("#menu-button");
+
+let current_down = null;
+
+menu_button.addEventListener("mousedown", (e) => {
+    if (current_down != null) current_down.classList.remove("down");
+    current_down = menu_button;
+
+    current_down.classList.add("down");
+
+    e.stopPropagation();
+});
+
+document.addEventListener("mouseup", (e) => {
+    if (current_down != null) current_down.classList.remove("down");
+});
+
+document.querySelector("#avatar-btn").addEventListener("click", (e) => {
+    console.log(e);
+});*/
